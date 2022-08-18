9603c6abfccb4fd24b1192b6338c881269ae40ab
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-08-17 20:40:03 -0500

    please

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,13 +5,13 @@ const {
   validateAuthentication,
 } = require("../middlewares/validation");
 
-const { NotFoundError } = require("../errors/not-found-error");
+// const { NotFoundError } = require("../errors/not-found-error");
 
-// router.post("/signin", validateAuthentication, login);
-// router.post("/signup", validateUserBody, createUser);
+router.post("/signin", validateAuthentication, login);
+router.post("/signup", validateUserBody, createUser);
 
-router.use((req, res, next) => {
-  next(new NotFoundError("No page found for this route"));
-});
+// router.use((req, res, next) => {
+//   next(new NotFoundError("No page found for this route"));
+// });
 
 module.exports = router;
