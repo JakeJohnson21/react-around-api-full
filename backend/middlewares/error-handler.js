dbdadcd5e3f93f436e40df74b9438300d4abb362
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-08-17 20:21:35 -0500

    ugh2

--- backend/middlewares/error-handler.js
+++ backend/middlewares/error-handler.js
@@ -3,7 +3,7 @@ const errorHandler = (err, req, res, next) => {
   res.status(err.statusCode).send({
     message: statusCode === 500 ? "An error occured on the server" : message,
   });
-  next();
+  return next();
 };
 
 module.exports = errorHandler;
