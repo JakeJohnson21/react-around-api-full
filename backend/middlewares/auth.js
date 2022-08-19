906691a8d49953474668445d3e3e9db50a6c883b
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-08-19 16:08:23 -0500

    hello 123

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,14 +1,15 @@
 const { UnauthorizedError } = require("../errors/unauthorized-error");
 const { JWT_SECRET } = require("../utils/config");
-
+console.log("hello1");
 const auth = (req, res, next) => {
+  console.log("hello2");
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith("bearer ")) {
     return next(new UnauthorizedError("Authorization required"));
   }
   const token = authorization.replace("Bearer ", "");
   let payload;
-
+  console.log("hello3");
   try {
     payload = token.verify(token, JWT_SECRET);
   } catch (err) {
