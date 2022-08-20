30b4a983a1413b67fff53f75b0f4a8d766be426d
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-08-19 19:33:05 -0500

    sadfa

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -12,7 +12,7 @@ const {
   unlikeCard,
 } = require("../controllers/cards");
 
-router.post("/", validateCardBdody, createCard);
+router.post("/", validateCardBody, createCard);
 router.delete("/:id", validateAuthentication, deleteCard);
 router.put("/:cardId/likes", validateAuthentication, likeCard);
 router.delete("/:cardId/likes", validateAuthentication, unlikeCard);
