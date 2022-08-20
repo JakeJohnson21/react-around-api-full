3b0f7d4f0701be34d61f97d8a2927b6512ef8ad2
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-08-20 12:39:20 -0500

    console logging middlewares/auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,27 +2,29 @@ const jwt = require("jsonwebtoken");
 const UnauthorizedError = require("../errors/unauthorized-error");
 const JWT_SECRET = require("../utils/config");
 
-console.log("hello1");
+// console.log("hello1");
 const auth = (req, res, next) => {
-  console.log("hello2");
+  // console.log("hello2");
   const { authorization } = req.headers;
-  console.log(authorization, authorization.startsWith("Bearer "));
+  console.log(authorization);
   if (!authorization || !authorization.startsWith("Bearer ")) {
     return next(new UnauthorizedError("Authorization required"));
   }
 
   const token = authorization.replace("Bearer ", "");
   let payload;
-  console.log("hello3");
+  // console.log("hello3");
   try {
-    console.log(token, JWT_SECRET);
+    // console.log(token, JWT_SECRET);
     payload = jwt.verify(token, JWT_SECRET);
     console.log("payload: ", payload);
   } catch (err) {
     return next(new UnauthorizedError("Authorization required"));
   }
-  console.log(req.user);
+  // console.log(req.user);
   req.user = payload;
+  console.log("payload: ", payload);
+  console.log("req.headers ", req.headers);
 
   return next();
 };
