b9757fd2170b863e1b58fecea3116edd7bfd675c
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-09-08 23:07:35 -0500

    _id for update avat/user

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -91,7 +91,7 @@ const createUser = (req, res, next) => {
 const updateUser = (req, res, next) => {
   const { id } = req.user;
   const { name, about } = req.body;
-  console.log(" #1 id / name , about: ", id, name, about);
+  console.log(" #1 id / name , about: ", req.user._id, name, about);
   User.findByIdAndUpdate(
     id,
     { name, about },
@@ -99,7 +99,7 @@ const updateUser = (req, res, next) => {
   )
     .orFail(() => new NotFoundError("No user with that id was found"))
     .then((user) => {
-      console.log(" #2 id / name , about: ", id, name, about);
+      console.log(" #2 id / name , about: ", req.user._id, name, about);
       res.send({ data: user });
     })
     .catch(next);
@@ -107,12 +107,12 @@ const updateUser = (req, res, next) => {
 const updateAvatar = (req, res, next) => {
   const { id } = req.user;
   const { avatar } = req.body;
-  console.log(" #1 id / avatar: ", req.user.id, avatar);
+  console.log(" #1 id / avatar: ", req.user._id, avatar);
   User.findByIdAndUpdate(id, avatar, { new: true, runValidators: true })
 
     .orFail(() => new NotFoundError("No user found with that ID"))
     .then((user) => {
-      console.log(" #2 id / avatar: ", req.user.id, avatar);
+      console.log(" #2 id / avatar: ", req.user._id, avatar);
       res.status(201).send({ data: user });
     })
     .catch(next);
