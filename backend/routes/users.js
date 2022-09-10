617939026e5623a7b772378b055259f0cac3a906
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-09-09 20:21:17 -0500

    remove validators from users patch routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -12,8 +12,8 @@ const {
   getCurrentUser,
 } = require("../controllers/users");
 
-router.patch("/me", validateProfile, updateUser);
-router.patch("/me/avatar", validateAvatar, updateAvatar);
+router.patch("/me", updateUser);
+router.patch("/me/avatar", updateAvatar);
 router.get("/", validateUserBody, getUsers);
 router.get("/me", getUser);
 router.get("/:_id", validateUserBody, getCurrentUser);
