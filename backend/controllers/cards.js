2d9fecb4bb325d8b6e1fc6817f29f74f650c73c8
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-09-11 00:19:43 -0500

    card.owner

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -65,7 +65,7 @@ const deleteCard = (req, res, next) => {
   Card.findById(cardId)
     .orFail(() => new NotFoundError("No card found by that id"))
     .then((card) => {
-      console.log("CARD OWNER: ", owner);
+      console.log("CARD OWNER: ", card.owner);
       if (!card.owner.equals(req.user._id)) {
         next(new ForbiddenError("You cannot delete someone else's card"));
       }
